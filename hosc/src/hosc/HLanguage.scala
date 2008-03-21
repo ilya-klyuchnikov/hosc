072f9fd1ad2fc305d38f5e1bdc1e29d26b6510d4
ilya.klyuchnikov <none@none>
2008-03-21 13:58:11 +0000

    Lambda Abstraction is added
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%405

--- hosc/src/hosc/HLanguage.scala
+++ hosc/src/hosc/HLanguage.scala
@@ -9,10 +9,10 @@ object HLanguage {
    sealed abstract class Term extends Expression
    case class Variable(name: String) extends Term
    case class Constructor(name: String, args: List[Term]) extends Term
-   
-   case class CaseExpression(selector: Term, branches: List[Branch]) extends Term   
    case class Call(name: String) extends Term
+   case class LambdaAbstraction(v: Variable, t: Term) extends Term
    case class Application(head: Term, arg: Term) extends Term
+   case class CaseExpression(selector: Term, branches: List[Branch]) extends Term   
    
    case class Branch(pattern: Pattern, term: Term)
    case class Pattern(name: String, args: List[Variable])
