c6533104ef695691ec0ef1abbb7ef7ad87357121
ilya.klyuchnikov <none@none>
2008-03-28 13:57:53 +0000

    HParsers is refactored
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%4010

--- hosc/src/hosc/HParsers.scala
+++ hosc/src/hosc/HParsers.scala
@@ -1,20 +1,10 @@
 package hosc
 
 import scala.util.parsing.input.Reader
-import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 
 import HLanguage._
 
-object HParsers extends StandardTokenParsers with StrongParsers {
-  private object startsWithLowerCase extends PartialFunction[String, String] {
-    def isDefinedAt(s: String) = s.charAt(0).isLowerCase
-    def apply(s: String) = s
-  }
-  
-  private object startsWithUpperCase extends PartialFunction[String, String] {
-    def isDefinedAt(s: String) = s.charAt(0).isUpperCase
-    def apply(s: String) = s
-  }
+object HParsers extends HTokenParsers with StrongParsers {
   
   lexical.delimiters += ("(", ")", ",", "=", ";", ":", "%", "{", "}")
   lexical.reserved += ("case", "of")
@@ -24,7 +14,7 @@ object HParsers extends StandardTokenParsers with StrongParsers {
   def term: Parser[Term] = chainl1(aterm, success(Application(_: Term, _: Term)))
   
   private def variable: Parser[Variable] = 
-   (ident ^? startsWithLowerCase) ^^ Variable
+   lident ^^ Variable
   
   private def lambdaAbstraction: Parser[LambdaAbstraction] =
     "%" ~> variable ~ ("{" ~> aterm <~ "}") ^^ {case v ~ t => LambdaAbstraction(v, t)}
@@ -36,12 +26,10 @@ object HParsers extends StandardTokenParsers with StrongParsers {
     pattern ~ (":" ~> aterm <~ ";") ^^ {case p ~ t => Branch(p, t)}
   
   private def pattern: Parser[Pattern] =
-    (ident ^? (startsWithUpperCase, x => "Expected pattern, found " + x)) ~ (variable*) ^^
-      {case name ~ args => Pattern(name, args)}
+    uident ~ (variable*) ^^ {case name ~ args => Pattern(name, args)}
 
   private def constructor: Parser[Constructor] =
-    (ident ^? (startsWithUpperCase, x => "Expected constructor, found " + x)) ~ (aterm*) ^^
-      {case name ~ args => Constructor(name, args)}
+    uident ~ (aterm*) ^^ {case name ~ args => Constructor(name, args)}
   
   def parseTerm(r: Reader[Char]): ParseResult[Term] = strong(term, "term or <eof> expected") (new lexical.Scanner(r))
 }
