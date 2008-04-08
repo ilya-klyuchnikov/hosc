ae80630b51a3171e4d40aede3c2c18acd3166811
ilya.klyuchnikov <none@none>
2008-04-08 12:28:39 +0000

    refactoring
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%4040

--- hosc/src/hosc/Validator.scala
+++ hosc/src/hosc/Validator.scala
@@ -1,5 +1,7 @@
 package hosc
 
+import scala.util.parsing.input.Positional
+
 import HLanguage._
 import HParsers._
 
@@ -18,41 +20,39 @@ object Validator {
       
       def valTypeUsage(t: Type, allowFreeVars: Boolean): Unit = t match {
         case tv: TypeVariable => {
-          if (!allowFreeVars && !(tvs contains tv)) throw ValidatorError(error("undefined var " + tv.name, tv))
+          if (!allowFreeVars && !(tvs contains tv)) err("undefined var " + tv.name, tv)
           tvsUsed += tv
         }
         case Arrow(t1, t2) => valTypeUsage(t1, allowFreeVars); valTypeUsage(t2, allowFreeVars); 
         case tc @ TypeConstructor(n, args) => {
           p.getTypeConstructorDefinition(n) match {
             case Some(td) => {
-              if (td.args.length != args.length) 
-                throw ValidatorError(error("wrong numbers of argument for type " + n, tc))
+              if (td.args.length != args.length) err("wrong numbers of argument for type " + n, tc)
               for (a <- args) valTypeUsage(a, allowFreeVars)
             }
-            case None => throw ValidatorError(error("unknown type " + n, tc))
+            case None => err("unknown type " + n, tc)
           }
         }
       }
       
       def valDC(dc: DataConstructor) = {
-        if (dataConNames contains dc.name) 
-          throw ValidatorError(error("duplicate data constructor definition " + dc.name, dc))
+        if (dataConNames contains dc.name) err("duplicate data constructor definition " + dc.name, dc)
         dataConNames += dc.name
         for (a <- dc.args) valTypeUsage(a, false)
       }
       
-      if (typeNames contains td.name) throw ValidatorError(error("duplicate type name " + td.name, td))
+      if (typeNames contains td.name) err("duplicate type name " + td.name, td)
       typeNames += td.name
       
       td match {
         case tcd: TypeConstructorDefinition => {
           for (v <- tcd.args) {
-            if (tvs contains v) throw ValidatorError(error("duplicate type variable " + v.name, v))
+            if (tvs contains v) err("duplicate type variable " + v.name, v)
             tvs += v
           }
           for (dc <- tcd.cons) valDC(dc)
           tcd.args find {!tvsUsed.contains(_)} match {
-            case Some(tv) => throw ValidatorError(error("useless type variable " + tv.name, tv))
+            case Some(tv) => err("useless type variable " + tv.name, tv)
             case None =>
           }
         }
@@ -65,59 +65,49 @@ object Validator {
       val pat = c.branches.head.pattern
       val dcn = pat.name
       p.getTypeDefinitionForDC(dcn) match {
-        case None => throw ValidatorError(error("undefined constructor " + dcn, pat))
+        case None => err("undefined constructor " + dcn, pat)
         case Some(td) => {
           val consNames = Set.empty[String] ++ (td.cons map (_.name))
           var usedNames = Set.empty[String]
           for (b <- c.branches){
-            val pattern = b.pattern
-            if (!(consNames contains pattern.name))
-              throw ValidatorError(error("type " + td.name +" doesn't define constructor " + pattern.name, pattern))
-            val dc = p.getDataConstructor(pattern.name).get
-            if (dc.args.length != pattern.args.length) 
-              throw ValidatorError(error("wrong number of parameters for constructor " + pattern.name, pattern))
-            if (usedNames contains dc.name)
-              throw ValidatorError(error("duplicate " + pattern.name, pattern))
-            usedNames += pattern.name
+            val pt = b.pattern
+            if (!(consNames contains pt.name)) err("type " + td.name +" doesn't define constructor " + pt.name, pt)
+            val dc = p.getDataConstructor(pt.name).get
+            if (dc.args.length != pt.args.length) err("wrong number of parameters for constructor " + pt.name, pt)
+            if (usedNames contains dc.name) err("duplicate " + pt.name, pt)
+            usedNames += pt.name
             var pVars = Set.empty[String]
-            for (v <- pattern.args){
-              if (pVars contains v.name)
-                throw ValidatorError(error("duplicate variable " + v.name + " in pattern", v))
+            for (v <- pt.args){
+              if (pVars contains v.name) err("duplicate variable " + v.name + " in pattern", v)
               pVars += v.name
             }
             valTerm(boundedVars ++ pVars, b.term)
           }
           val unused = consNames -- usedNames
-          if (!(unused isEmpty)) {
-            throw ValidatorError(error("case is not exhaustive. missing pattern(s) " + unused.mkString(", "), c.selector))
-          }
+          if (!(unused isEmpty)) err("case is not exhaustive. missing pattern(s) " + unused.mkString(", "), c.selector)
         }
       }
     }
     
     def valTerm(boundedVars: Set[String], term: Term): Unit = term match{
       case v: Variable => 
-        if (!(boundedVars contains v.name)) throw ValidatorError(error("unbounded variable " + v.name, v))
+        if (!(boundedVars contains v.name)) err("unbounded variable " + v.name, v)
       case c: Constructor => {
         p.getDataConstructor(c.name) match {
           case Some(dc) => {
-            if (dc.args.length != c.args.length) 
-              throw ValidatorError(error("wrong number of parameters for constructor " + c.name, c))
-            for (arg <- c.args)
-              valTerm(boundedVars, arg)
+            if (dc.args.length != c.args.length) err("wrong number of parameters for constructor " + c.name, c)
+            for (arg <- c.args) valTerm(boundedVars, arg)
           }
-          case None => throw ValidatorError(error("undefined constructor " + c.name, c))
+          case None => err("undefined constructor " + c.name, c)
         }
-        null
       }
       case l: LambdaAbstraction => valTerm(boundedVars + l.v.name, l.t)
       case a: Application => {valTerm(boundedVars, a.head); valTerm(boundedVars, a.arg);}
       case c: CaseExpression => valCase(boundedVars, c)
-    }
-    
+    }    
     
     def valFD(f: Function) = {
-      if (fNames contains f.name) throw ValidatorError(error("duplicate function " + f.name, f))
+      if (fNames contains f.name) err("duplicate function " + f.name, f)
       valTerm(allFNames, f.lam)
     }
     
@@ -128,6 +118,10 @@ object Validator {
     } catch {
       case ValidatorError(he) => he
     }
+  }
+  
+  def err(msg: String, pos: Positional) = {
+    throw ValidatorError(error(msg, pos))
   }
   
 }
