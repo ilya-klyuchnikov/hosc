2889a44705dfce3f323b8461175512d146e01525
ilya.klyuchnikov <none@none>
2008-04-17 16:40:57 +0000

    fix
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%4081

--- hosc/src/hosc/TermAlgebra.scala
+++ hosc/src/hosc/TermAlgebra.scala
@@ -403,7 +403,7 @@ object TermAlgebra {
     var term = g.term
     for (s <- g.sub1) term = applySubstitution(term, Map(s))
     
-    val newS = (g.sub1 zip g.sub2) map {p => (p._1._2.asInstanceOf[Variable], p._2._2)}
+    var newS = ((g.sub1 zip g.sub2) map {p => (p._1._2.asInstanceOf[Variable], p._2._2)}) remove (p => p._1 == p._2)
     Generalization(term, Nil, newS)    
   }
   
