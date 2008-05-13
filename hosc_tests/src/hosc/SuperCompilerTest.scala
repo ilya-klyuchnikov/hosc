88980c2c18bd49f1214d44eb02aeb9f6978b780d
ilya.klyuchnikov <none@none>
2008-05-13 15:52:20 +0000

    Supercompiler tests are refactored
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40122

--- hosc_tests/src/hosc/SuperCompilerTest.scala
+++ hosc_tests/src/hosc/SuperCompilerTest.scala
@@ -8,83 +8,94 @@ import Util._
 
 class SuperCompilerTest {
   
-  @Ignore
   @Test def simple(): Unit = {
-    testSC("input/revInt.hl", "app (app x y) z")
-    testSC("input/revInt.hl", "app (app x y) x")
-    testSC("input/revInt.hl", "app x x")
+    testSC("hl/supercompiler/rev1.hl")
+    testSC("hl/supercompiler/rev2.hl")
+    testSC("hl/supercompiler/rev3.hl")
   }  
   
-  def testSC(fileName: String, input: String): Unit = {
+  def testSC(fileName: String): Unit = {
     val p = programFromFile(fileName)
     val sc = new SuperCompiler(p)
-    val term = termFromString(input, p)
-    val tree = sc.buildProcessTree(term)
+    val tree = sc.buildProcessTree(p.goal)
     println(tree)
   }
   
   @Test def processSamples(): Unit =
   {
     
-    SuperCompilerApp.main(Array("-i", "input/revInt.hl",
-                                "-e", "app (app x y) z", 
-                                "-t", "output/app1_1.svg",
-                                "-p", "output/app1.hl"));
-    SuperCompilerApp.main(Array("-i", "input/revInt.hl",
-        "-e", "app (app x y) x", 
-        "-t", "output/app2_1.svg",
-        "-p", "output/app2.hl"));
-    SuperCompilerApp.main(Array("-i", "input/revInt.hl",
-        "-e", "app x x", 
-        "-t", "output/app3_1.svg",
-        "-p", "output/app3.hl"));
-    SuperCompilerApp.main(Array("-i", "input/revInt.hl",
-        "-e", "rev x", 
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/rev.hl",
         "-t", "output/rev_1.svg",
         "-p", "output/rev.hl"));
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/rev1.hl",
+        "-t", "output/app1_1.svg",
+        "-p", "output/app1.hl"));
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/rev2.hl",
+        "-t", "output/app2_1.svg",
+        "-p", "output/app2.hl"));    
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/rev3.hl",
+        "-t", "output/app3_1.svg",
+        "-p", "output/app3.hl"));
     
-    SuperCompilerApp.main(Array("-i", "input/z01.hl",
-        "-e", "takenm n m", 
-        "-t", "output/z01.svg",
-        "-p", "output/z01.hl"));
-    SuperCompilerApp.main(Array("-i", "input/z02.hl",
-        "-e", "app x y", 
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/app.hl",
         "-t", "output/z02.svg",
         "-p", "output/z02.hl"));
         
-   SuperCompilerApp.main(Array("-i", "input/z03.hl",
-            "-e", "f1 (f x)", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/f.hl",
             "-t", "output/z03.svg",
             "-p", "output/z03.hl"));   
    
-   SuperCompilerApp.main(Array("-i", "input/mapNotNot.hl",
-       "-e", "mapNotNot y", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/mapNotNot1.hl", 
        "-t", "output/mapNotNot.svg",
        "-p", "output/mapNotNot.hl"));
    
-   SuperCompilerApp.main(Array("-i", "input/mapNotNot.hl",
-       "-e", "not (not y)", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/mapNotNot2.hl",
        "-t", "output/notNot.svg",
        "-p", "output/notNot.hl"));
    
-   SuperCompilerApp.main(Array("-i", "input/mapNotNot.hl",
-       "-e", "map not y", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/mapNotNot3.hl",
        "-t", "output/mapNot.svg",
        "-p", "output/mapNot.hl"));
    
-   SuperCompilerApp.main(Array("-i", "input/mapNotNot.hl",
-       "-e", "map x (Cons True (Cons False Nil)) ", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/mapNotNot4.hl",
        "-t", "output/mapx.svg",
        "-p", "output/mapx.hl"));
    
-   SuperCompilerApp.main(Array("-i", "input/mapNotNot.hl",
-       "-e", "id y", 
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/mapNotNot5.hl",
        "-t", "output/id.svg",
        "-p", "output/id.hl"));
    
-   SuperCompilerApp.main(Array("-i", "input/eqnum.hl",
-       "-e", "eqnum c c", 
-       "-t", "output/eqnum.svg",
-       "-p", "output/eqnum.hl"));
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/eqnumxx.hl",
+       "-t", "output/eqnumxx.svg",
+       "-p", "output/eqnumxx.hl"));
+   
+   SuperCompilerApp.main(Array("-si", "hl/supercompiler/app1.hl",
+       "-t", "output/app1.svg",
+       "-p", "output/app.hl"));
   }
+  
+  @Test def takenm(): Unit = {
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/takenm.hl",
+        "-t", "output/takenm.svg",
+        "-p", "output/takenm.hl"));
+  }
+  
+  @Test def eqnum(): Unit = {
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/eqnum.hl",
+        "-t", "output/eqnum.svg",
+        "-p", "output/eqnum.hl"));
+  }
+  
+  @Test def synapse(): Unit = {
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/synapse.hl",
+        "-t", "output/synapse.svg",
+        "-p", "output/synapse.hl"));
+  }
+  
+  @Test def regexp(): Unit = {
+    SuperCompilerApp.main(Array("-si", "hl/supercompiler/regexp.hl",
+        "-t", "output/regexp.svg",
+        "-p", "output/regexp.hl"));
+  }
+  
 }
