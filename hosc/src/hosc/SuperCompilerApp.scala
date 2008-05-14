9478056654cd1a02c557cb270cb462047b8a8d05
ilya.klyuchnikov <none@none>
2008-05-14 08:29:17 +0000

    refactoring
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40123

--- hosc/src/hosc/SuperCompilerApp.scala
+++ hosc/src/hosc/SuperCompilerApp.scala
@@ -15,34 +15,39 @@ import HParsers._
 import Util._
 
 object SuperCompilerApp {
-  val help = """usage: hosc.SuperCompilerApp -i input_file -e expression -t tree_output_file -p program_output_file
+  val help = """usage: hosc.SuperCompilerApp [-si sinput_file | -i input_file] -t tree_output_file -p program_output_file
   |Where:
-  |input_file            path to input file (relative or absolute)
-  |expression            term to be supercompiled
+  |sinput_file           path to input file where code is written using syntactic sugar
+  |input_file            path to input file where code is written using standard syntax
   |tree_output_file      path to file where process tree will be placed (in SVG format)
   |program_output_file   path to file where residual program will be placed
   |""".stripMargin
   def main(args : Array[String]) : Unit = {
     var fileName: String = null
-    var termString: String = null
     var outFileName: String = null
     var outProgramFileName: String = null
+    var sugared = false
     args.toList match {
-      case "-i" :: input_file :: "-e" :: expr :: "-t" :: output_file :: "-p" :: output_file_1 :: Nil =>
+      case "-i" :: input_file :: "-t" :: output_file :: "-p" :: output_file_1 :: Nil =>
         fileName = input_file
-        termString = expr
         outFileName = output_file
         outProgramFileName = output_file_1
+        throw new IllegalArgumentException("Standard input is currently unsupported")
+      case "-si" :: input_file :: "-t" :: output_file :: "-p" :: output_file_1 :: Nil =>
+        fileName = input_file
+        outFileName = output_file
+        outProgramFileName = output_file_1
+        sugared = true
       case "-help" :: Nil => 
         println(help)
         return
       case _ => 
         throw new IllegalArgumentException("run spcs.SuperCompilerApp -help for help")       
     }
+    
     val program = programFromFile(fileName)
     val sc = new SuperCompiler(program)
-    val term = termFromString(termString, program)
-    val pt = sc.buildProcessTree(term)    
+    val pt = sc.buildProcessTree(program.goal)    
     val svg = new ProcessTreeSVG(pt).treeToSVG
     
     val svgFile = new java.io.File(outFileName)
@@ -61,7 +66,6 @@ object SuperCompilerApp {
     val fw = new FileWriter(slFile);
     doc.format(100, fw)
     fw.flush();
-    fw.close();
-    
+    fw.close();    
   }
 }
