57b51b751ac3e648495a8fcb7e1738da418498c9
ilya.klyuchnikov <none@none>
2008-06-01 08:15:02 +0000

    term algebra 1: context, redex, observable
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40143

--- hosc/src/hosc/TermAlgebra1.scala
+++ hosc/src/hosc/TermAlgebra1.scala
@@ -3,6 +3,45 @@ package hosc;
 import HLanguage1._
 
 object TermAlgebra1 {
+  var i = 0
+  def newVar1() = {
+    i += 1
+    Variable1("$" + i) 
+  }
+  type Substitution = Tuple2[Variable1, Term1]
+  type DoubleSubstitution = Tuple3[Variable1, Term1, Term1]
+  case class Generalization(term: Term1, sub1: List[Substitution], sub2: List[Substitution])
+  case class Generalization2(term: Term1, dSub: List[DoubleSubstitution])
+
+  sealed abstract class TermDecomposition1
+  
+  sealed abstract class Observable1(val term: Term1) extends TermDecomposition1
+  case class ObservableVar(v: Variable1) extends Observable1(v)
+  case class ObservableVarApp(v: Variable1, app: Application1) extends Observable1(app)
+  case class ObservableCon1(c: Constructor1) extends Observable1(c)
+  case class ObservableLam1(l: LambdaAbstraction1) extends Observable1(l)
+  
+  sealed abstract class Redex1(val term : Term1)
+  case class RedexCall1(v: Variable1) extends Redex1(v)
+  case class RedexLamApp1(lam: LambdaAbstraction1, app: Application1) extends Redex1(app)
+  case class RedexCaseVarApp1(a: Application1, ce: CaseExpression1) extends Redex1(ce)
+  case class RedexCaseVar1(v: Variable1, ce: CaseExpression1) extends Redex1(ce)  
+  case class RedexCaseCon1(c: Constructor1, ce: CaseExpression1) extends Redex1(ce)
+  
+  sealed abstract class Context1(val redex: Redex1) extends TermDecomposition1 {
+    def replaceHole(t: Term1): Term1
+  }  
+  case class ContextHole1(override val redex: Redex1) extends Context1(redex) {
+    def replaceHole(t: Term1) = t
+  }
+  // app = con e 
+  case class ContextApp1(head: Context1, app: Application1) extends Context1(head.redex) {
+    def replaceHole(t: Term1) = Application1(head.replaceHole(t), app.arg)
+  }
+  // ce = case selector of ....
+  case class ContextCase1(selector: Context1, ce: CaseExpression1) extends Context1(selector.redex) {
+    def replaceHole(t: Term1) = CaseExpression1(selector.replaceHole(t), ce.branches)
+  } 
   
   def applySubstitution1(term: Term1, s: Map[Variable1, Term1]): Term1 = term match {
     case v: Variable1 => s.get(v) match {case Some(t) => t; case None => v}
