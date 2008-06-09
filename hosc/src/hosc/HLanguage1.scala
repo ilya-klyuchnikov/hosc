a6046532f53472ee9ab88f962a2daffd2c4113fa
ilya.klyuchnikov <none@none>
2008-06-09 06:53:31 +0000

    loop/repeat construct is introduced
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40162

--- hosc/src/hosc/HLanguage1.scala
+++ hosc/src/hosc/HLanguage1.scala
@@ -5,20 +5,30 @@ import scala.text.Document._
 
 object HLanguage1 {
    val ED: scala.text.Document = empty
-  
+   sealed abstract class Label 
+   case class Loop extends Label
+   case class Repeat extends Label
+   
    sealed abstract class Expression1 {
      def toDoc: Document
    }
-   sealed abstract class Term1 extends Expression1
+   sealed abstract class Term1 extends Expression1 {
+     var label : Label = null
+     def labelToString = label match {
+       case Loop() => "Loop: "
+       case Repeat() => "Repeat: "
+       case null => ""
+     }
+   }
    
    case class Variable1(name: String) extends Term1 {
      var call = false
-     override def toString = name
+     override def toString = labelToString + name
      def toDoc = text(name)
    }
    
    case class Constructor1(name: String, args: List[Term1]) extends Term1 {
-     override def toString = "(" + name + " " + args.mkString(" ") + ")";     
+     override def toString = labelToString + "(" + name + " " + args.mkString(" ") + ")";     
      def toDoc = args match {
        case Nil => text(name)
        case _ => group(("(" + name) :: nest(2, ED :: args.foldRight(ED){(x, y) => ED :/: x.toDoc :: y}) :: ")" :: ED)
@@ -26,23 +36,23 @@ object HLanguage1 {
    }
    
    case class LambdaAbstraction1(v: Variable1, t: Term1) extends Term1 {
-     override def toString = "%" + v.name + " {" + t + "}";
+     override def toString = labelToString + "%" + v.name + " {" + t + "}";
      def toDoc = "%" :: v.toDoc :: " {" :: nest(2, ED :/: t.toDoc) :/: "}" :: ED 
    }
    
    case class Application1(head: Term1, arg: Term1) extends Term1 {
-     override def toString = "(" + head + " " + arg + ")";
+     override def toString = labelToString + "(" + head + " " + arg + ")";
      def toDoc = group("(" :: nest(2, head.toDoc :/: arg.toDoc ) :: ")" :: ED)
    }
    
    case class CaseExpression1(selector: Term1, branches: List[Branch1]) extends Term1 {
-     override def toString = "case (" + selector + ") of " + branches.mkString("{", " ", "}")
+     override def toString = labelToString + "case (" + selector + ") of " + branches.mkString("{", " ", "}")
      def toDoc = group( group("case " :/: selector.toDoc :/: " of {" :: ED) :: 
        nest(2, branches.foldRight(ED){(b, y) => ED :/: b.toDoc :: y}) :/: "}" :: ED)
    }
    
    case class LetRecExpression1(binding: Pair[Variable1, Term1], expr: Term1) extends Term1 {
-     override def toString = "letrec " + (binding._1 + "=" + binding._2) + " in " + expr;
+     override def toString = labelToString + "letrec " + (binding._1 + "=" + binding._2) + " in " + expr;
      def toDoc = group("letrec" :: 
          nest(2, group (ED :/: binding._1.toDoc :: " = " :: binding._2.toDoc))
          :/: "in " :: nest(2, ED :/: expr.toDoc))
