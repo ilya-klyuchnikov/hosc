a0a0e0dbaa5b781d8b5834d84bf25d3c3feb41cb
ilya.klyuchnikov <none@none>
2008-06-10 14:02:13 +0000

    supercompiler1 stub
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40191

--- hosc/src/hosc/SuperCompiler1.scala
+++ hosc/src/hosc/SuperCompiler1.scala
@@ -1,38 +1,27 @@
 package hosc;
 
 //import HLanguage._
+import HLanguage._
 import HLanguage1._
 import TermAlgebra1._
 import ProcessTree1._
+import HLUtils._
 
-abstract class SuperCompiler1(val program: Program1) {
+class SuperCompiler1(program: Program) {
+  val scProgram: Program = {
+    val pt = new SuperCompiler(program).buildProcessTree(program.goal)
+    val generator = new ResidualProgramGenerator(program, pt)
+    val p1 = hl1ToHl(generator.generateProgram())
+    Postprocessor.postprocess(p1)
+    p1
+  }
   val emptyMap = Map[Variable1, Term1]()
   
-  def driveExp(expr: Expression1): List[Pair[Term1, Map[Variable1, Term1]]] = expr match {
-    case LetExpression1(bs, t) => (t, Map[Variable1, Term1]()) :: (bs map {b => (b._2, emptyMap)}) 
-    case t: Term1 => decompose1(t) match {
-      case ObservableVar1(_) => Nil
-      case ObservableCon1(c) => c.args map {a => (a, emptyMap)}
-      case ObservableVarApp1(_, app) => extractAppArgs1(app) map {a => (a, emptyMap)}
-      case ObservableLam1(l) => (l.t, emptyMap) :: Nil
-      case context: Context1 => context.redex match {
-        case RedexLamApp1(lam, app) => 
-          (context.replaceHole(lam.t/Map(lam.v -> app.arg)), emptyMap) :: Nil
-        case RedexCaseCon1(c, ce) => {
-          val b = ce.branches.find(_.pattern.name == c.name).get
-          val sub = Map[Variable1, Term1]() ++ (b.pattern.args zip c.args)
-          (context.replaceHole(b.term/sub), emptyMap) :: Nil          
-        }
-        case RedexCaseVar1(v, CaseExpression1(sel, bs)) =>
-          (sel, emptyMap) :: (bs map 
-            {b => (replaceTerm1(context.replaceHole(b.term), v, Constructor1(b.pattern.name, b.pattern.args)), emptyMap)})  
-        case RedexCaseVarApp1(a, CaseExpression1(sel, bs)) =>
-          (sel, emptyMap) :: (bs map 
-            {b => (replaceTerm1(context.replaceHole(b.term), a, Constructor1(b.pattern.name, b.pattern.args)), emptyMap)})
-        case RedexLetRec1(letrec) => (context.replaceHole(unfold(letrec)), emptyMap) :: Nil
-      }
-    }
+  def superCompile() = {
+    val tree = ProcessTree1(hlToHl1(scProgram.goal))
+    val transformer = new Transformer(tree, scProgram)
+    transformer.transform()
+    tree
   }
   
-  def unfold(letrec: LetRecExpression1): Term1
 }
