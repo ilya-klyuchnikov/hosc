c2402d292f1d6a0744a013bb4b02a6d5c884c6ea
ilya.klyuchnikov <none@none>
2008-06-10 10:27:37 +0000

    code constructor for new supercompiler
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40183

---
+++ hosc/src/hosc/CodeConstructor.scala
@@ -0,0 +1,124 @@
+package hosc;
+
+import HLanguage1._
+import ProcessTree1._
+import TermAlgebra1._
+import MSG1._
+
+class CodeConstructor(val tree: ProcessTree1) {
+  var i = 0
+  def createFName() = {
+    i += 1
+    "f$" + i
+  }
+  
+  def construct(node: Node1): Term1 = node.expr match {
+    case LetExpression1(bindings, _) => {
+      construct(node.children.head)/Map(bindings:_*)
+    }
+    case term: Term1 => term.label match { 
+      case Loop() => node.repeatedOf match {
+        case null => {
+          val repeatNodes = tree.leafs filter {_.repeatedOf == node}
+          var vars = Set[Variable1]()
+          for (n <- repeatNodes) {
+            val repeatTerm = n.expr.asInstanceOf[Term1]
+            val msg = strongMsg(term, repeatTerm)
+            val args0 = msg.sub2 map {p => p._1}
+            vars = vars ++ args0
+          }
+          val fargs = vars.toList
+          val appHead = Variable1(createFName())
+          appHead.call = true
+          node.signature = constructApplication1(appHead, fargs)
+          val freshVars = fargs map {x => newVar1()}
+          val sub = Map((fargs zip freshVars):_*)
+          val lambdaBody = construct(node.children.head)/sub
+          val lambda = constructLambda1(freshVars, lambdaBody)
+          LetRecExpression1((appHead, lambda), node.signature)
+        }
+        case parentNode => {
+          val parentTerm = parentNode.expr.asInstanceOf[Term1]          
+          val msg = strongMsg(parentTerm, term)
+          parentNode.signature/Map(msg.sub2:_*)
+        }
+      }
+      case Repeat() => construct(node.children.head)
+      case null =>  decompose1(term) match {
+        case ObservableVar1(v) => v
+        case ObservableCon1(c) => Constructor1(c.name, node.children map construct)
+        case ObservableVarApp1(v, app) => constructApplication1(v, node.children map construct)
+        case ObservableLam1(lambda) => LambdaAbstraction1(lambda.v, construct(node.children.head))
+        case context: Context1 => context.redex match {
+          case RedexLamApp1(lam, app) => construct(node.children.head)
+          case RedexCaseCon1(c, ce) => construct(node.children.head)
+          case RedexCaseVar1(v, CaseExpression1(sel, bs)) => {
+            val branches = (bs zip node.children.tail) map {p => Branch1(p._1.pattern, construct(p._2))}
+            val selector = construct(node.children.head)
+            CaseExpression1(selector, branches)
+          }
+          case RedexCaseVarApp1(a, CaseExpression1(sel, bs)) => {
+            val branches = (bs zip node.children.tail) map {p => Branch1(p._1.pattern, construct(p._2))}
+            val selector = construct(node.children.head)
+            CaseExpression1(selector, branches)
+          }
+          case RedexLetRec1(letrec) => {
+            val letrecCallNode = node.children.head
+            tree.leafs filter {_.repeatedOf == letrecCallNode} match {
+              case Nil => construct(letrecCallNode.children.head) 
+              case repeatNodes => {
+                var vars = Set[Variable1]()
+                for (n <- repeatNodes) {
+                  val repeatTerm = n.expr.asInstanceOf[Term1]
+                  val msg = strongMsg(term, repeatTerm)
+                  val args0 = msg.sub2 map {p => p._1}
+                  vars = vars ++ args0
+                }
+                val fargs = vars.toList
+                val appHead = Variable1(createFName())
+                appHead.call = true
+                node.signature = constructApplication1(appHead, fargs)
+                val freshVars = fargs map {x => newVar1()}
+                val sub = Map((fargs zip freshVars):_*)
+                val lambdaBody = construct(letrecCallNode.children.head)/sub
+                val lambda = constructLambda1(freshVars, lambdaBody)
+                LetRecExpression1((appHead, lambda), node.signature)
+              }
+            }
+          }
+          case RedexCall1(f) => node.repeatedOf match {
+             case null => {
+               val code = construct(node.children.head)
+               code.label = Loop()
+               code
+             }
+             case parentNode => {
+               val letrecP: Boolean = parentNode.in match {
+                 case null => false
+                 case some => some.parent.expr match {
+                   case possibleLetrec: Term1 => decompose1(possibleLetrec) match {
+                     case possibleLetRecContext: Context1 => possibleLetRecContext.redex match {
+                       case RedexLetRec1(_) => true
+                       case _ => false
+                     }
+                     case _ => false
+                   }
+                   case _ => false
+                 }
+               }
+               if (letrecP) {
+                 val signature = parentNode.in.parent.signature
+                 val parentTerm = parentNode.expr.asInstanceOf[Term1]          
+                 val msg = strongMsg(parentTerm, term)
+                 signature/Map(msg.sub2:_*)
+               } else {
+                 term.label = Repeat()
+                 term
+               }
+             }
+          }
+        }
+      }    
+    }
+  }
+}
