201bc04a7c36cca37d541edfacb0477927e3af70
ilya.klyuchnikov <none@none>
2008-06-15 07:10:27 +0000

    code formatting
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40203

--- hosc/src/hosc/Ungeneralizer.scala
+++ hosc/src/hosc/Ungeneralizer.scala
@@ -1,9 +1,6 @@
 package hosc
 
 import HLanguage1._
-import ProcessTree1._
-import TermAlgebra1._
-import MSG1._
 
 class Ungeneralizer(tree: ProcessTree1, constructor: CodeConstructor) {
   def ungeneralize(): Boolean = {
@@ -12,7 +9,7 @@ class Ungeneralizer(tree: ProcessTree1, constructor: CodeConstructor) {
     var someWorkWasDone = false
     do {
       someWorkWasDone = false
-      val letchilds = tree.leafs filter {n => n.expr match {case l: LetRecExpression1 => true; case _ => false;}}
+      val letchilds = tree.leafs filter {_.expr match {case l: LetRecExpression1 => true; case _ => false;}}
       for (c <- letchilds) {
         c.ancestors() find {p => p.expr match {case l: LetExpression1 => true; case _ => false}} match {
           case Some(alpha) => {
@@ -23,7 +20,8 @@ class Ungeneralizer(tree: ProcessTree1, constructor: CodeConstructor) {
           }
           case None =>
         }
-      }} while (someWorkWasDone)
+      }
+    } while (someWorkWasDone)
     ungeneralized
   }
 }
