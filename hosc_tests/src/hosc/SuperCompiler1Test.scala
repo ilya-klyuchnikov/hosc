2802d534d1782fdc6de391eb9696f0075ce941ed
ilya.klyuchnikov <none@none>
2008-06-21 18:10:13 +0000

    more tests for sc1
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40225

--- hosc_tests/src/hosc/SuperCompiler1Test.scala
+++ hosc_tests/src/hosc/SuperCompiler1Test.scala
@@ -5,13 +5,13 @@ import org.junit.Assert._
 import HLanguage._
 import Util._
 class SuperCompiler1Test {
-  @Test def sc01(): Unit ={    
-    SuperCompilerApp1.main(Array("-si", "hl/sc1/sc01.hl",
-        "-t", "output/sc01.svg",
-        "-p", "output/sc01.hl"));
+  @Test def leqxplusxy(): Unit ={    
+    SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_leqxplusxy.hl",
+        "-t", "output/sc_leqxplusxy.svg",
+        "-p", "output/sc_leqxplusxy.hl"));
   }
-  
-  @Test def sc_even(): Unit ={    
+  @Test{val timeout = 20000}
+  def sc_even(): Unit ={    
     SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_even.hl",
         "-t", "output/sc_even.svg",
         "-p", "output/sc_even.hl"));
@@ -21,6 +21,12 @@ class SuperCompiler1Test {
     SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_even_plus.hl",
         "-t", "output/sc_even_plus.svg",
         "-p", "output/sc_even_plus.hl"));
+  }
+  
+  @Test def sc_leq_plusr(): Unit ={    
+    SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_leqplus.hl",
+        "-t", "output/sc_leqplus.svg",
+        "-p", "output/sc_leqplus.hl"));
   }
   
   @Test def sc_rev(): Unit ={    
@@ -38,5 +44,18 @@ class SuperCompiler1Test {
     SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_eqnum_plus1.hl",
         "-t", "output/sc_eqnum_plus1.svg",
         "-p", "output/sc_eqnum_plus1.hl"));
+  }
+  
+  @Test def sc_synapse(): Unit ={    
+    SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_synapse.hl",
+        "-t", "output/sc_synapse.svg",
+        "-p", "output/sc_synapse.hl"));
+  }
+  
+  @Test{val timeout = 10000}
+  def sc_exp(): Unit ={    
+    SuperCompilerApp1.main(Array("-si", "hl/sc1/sc_exp.hl",
+        "-t", "output/sc_exp.svg",
+        "-p", "output/sc_exp.hl"));
   }
 }
