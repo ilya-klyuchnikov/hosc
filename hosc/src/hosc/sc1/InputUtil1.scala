72a4c772522e3ef447839b0b0c00e06ffab27bae
ilya.klyuchnikov <none@none>
2008-07-16 10:21:06 +0000

    sc1 refactored
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40261

---
+++ hosc/src/hosc/sc1/InputUtil1.scala
@@ -0,0 +1,43 @@
+package hosc.sc1
+
+import scala.util.parsing.input.{CharArrayReader, Reader, StreamReader}
+import java.io.{BufferedReader, File, FileReader}
+import LangUtils._
+import hosc.util.Canonizer.{canonize1 => can}
+import HLanguage1._
+
+object InputUtil1 {
+  def program1FromString(input: String): Program1 = {
+    val pr = HParsers1.parseProgram(new CharArrayReader(input.toCharArray))
+    if (pr.successful) {
+      val program = pr.get
+      val canExpr = can(program.expr)      
+      val canProgram = Program1(program.ts, canExpr)
+      Postprocessor1.postprocess(canProgram)
+      val elcExpr = LangUtils.hl1ToELC(canExpr)
+      val typeInferrer = new TypeInferrer(canProgram.ts)
+      typeInferrer.inferType(elcExpr)
+      canProgram
+    } else { 
+      throw new IllegalArgumentException(pr.toString)
+    }
+  }
+  
+  def program1FromFile(fileName: String) = {
+    val file = new File(fileName)
+    val pr = HParsers1.parseProgram(StreamReader(new FileReader(fileName)))
+    if (pr.successful) {
+      val program = pr.get
+      val canExpr = can(program.expr)      
+      val canProgram = Program1(program.ts, canExpr)
+      Postprocessor1.postprocess(canProgram)
+      val elcExpr = LangUtils.hl1ToELC(canExpr)
+      val typeInferrer = new TypeInferrer(canProgram.ts)
+      typeInferrer.inferType(elcExpr)
+      canProgram
+    } else {
+      println(pr)
+      throw new IllegalArgumentException(pr.toString)
+    }
+  }
+}
