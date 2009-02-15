22337a69b051d39e332bba73a525220970277756
ilya.klyuchnikov <none@none>
2009-02-15 09:40:01 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40316

---
+++ hosc_app/main.py
@@ -0,0 +1,23 @@
+from hosc import views
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+application = webapp.WSGIApplication(
+                                     [('/', views.Root),
+                                      ('/all', views.All),
+                                      ('/supercompiler', views.Supercompiler),
+                                      ('/edit', views.Edit),
+                                      ('/delete', views.Delete),
+                                      ('/view', views.Get),
+                                      ('/svg', views.Svg),
+                                      ('/svgpreview', views.SvgPreview),
+                                      ('/authors', views.Authors),
+                                      ('/author', views.Author),
+                                      ('/mine', views.Mine)]
+                                     )
+
+def main():
+  util.run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
