97a878b897e6c8e13e1ca45dfaa70377b8cac9d4
ilya.klyuchnikov <none@none>
2009-02-17 15:48:05 +0000

    deleting obsolete code
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40331

--- hosc_tests/src/hosc/AllHoscTests.java
+++ hosc_tests/src/hosc/AllHoscTests.java
@@ -16,12 +16,11 @@ public class AllHoscTests {
 		suite.addTest(new JUnit4TestAdapter(hosc.MSGTest.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.TermAlgebraTest.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.HE1Test.class));
-		suite.addTest(new JUnit4TestAdapter(hosc.MSG1Test.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.Driver1Test.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.SuperCompilerTest.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.SuperCompiler0Test.class));
-		suite.addTest(new JUnit4TestAdapter(hosc.SuperCompiler1Test.class));
-		suite.addTest(new JUnit4TestAdapter(hosc.SuperCompiler2Test.class));
+		//suite.addTest(new JUnit4TestAdapter(hosc.SuperCompiler1Test.class));
+		//suite.addTest(new JUnit4TestAdapter(hosc.SuperCompiler2Test.class));
 		suite.addTest(new JUnit4TestAdapter(hosc.TasksTest.class));
 		// $JUnit-END$
 		return suite;
