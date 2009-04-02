3373d69a5f69e38c16905d6e1ea70176a6cf3c68
ilya.klyuchnikov <none@none>
2009-04-02 06:16:45 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40387

--- hosc/src/hosc/Eq.scala
+++ hosc/src/hosc/Eq.scala
@@ -4,12 +4,12 @@ import HLanguage._
 
 object Eq {
   def equivalent(term1: Expression, term2: Expression): Boolean = {
-    val map1to2 = scala.collection.mutable.Map[Variable, Variable]()
-    val map2to1 = scala.collection.mutable.Map[Variable, Variable]()
+    val map12 = scala.collection.mutable.Map[Variable, Variable]()
+    val map21 = scala.collection.mutable.Map[Variable, Variable]()
     def eq1(t1: Expression, t2: Expression): Boolean = (t1, t2) match {
-      case (v1: Variable, v2: Variable) => (map1to2.get(v1), map2to1.get(v2)) match {
+      case (v1: Variable, v2: Variable) => (map12.get(v1), map21.get(v2)) match {
         case (Some(v3), Some(v4)) => v2 == v3 && v1 == v4
-        case (None, None) => map1to2(v1) = v2; map2to1(v2) = v1; true
+        case (None, None) => map12(v1) = v2; map21(v2) = v1; true
         case _ => false
       }
       case (Constructor(name1, args1), Constructor(name2, args2)) =>
@@ -19,16 +19,14 @@ object Eq {
       case (CaseExpression(sel1, bs1), CaseExpression(sel2, bs2)) => {
         val bs1s = bs1 sort compareB1
         val bs2s = bs2 sort compareB1
-        if (bs1s.head.pattern.name == bs2s.head.pattern.name){
-          eq1(sel1, sel2) && List.forall2(bs1s, bs2s){
-            (b1, b2) => List.forall2(b1.pattern.args, b2.pattern.args)(eq1) && eq1(b1.term, b2.term)
-          }
-        } else {
-          false
-        }
+        bs1s(0).pattern.name == bs2s(0).pattern.name && 
+          eq1(sel1, sel2) &&
+            List.forall2(bs1s, bs2s){
+              (b1, b2) => List.forall2(b1.pattern.args, b2.pattern.args)(eq1) && eq1(b1.term, b2.term)
+            }
       }
       case (LetRecExpression((f1, a1), e1), LetRecExpression((f2, a2), e2)) => {
-        map1to2(f1) = f2; map2to1(f2) = f1;
+        map12(f1) = f2; map21(f2) = f1;
         eq1(a1, a2) && eq1(e1, e2)
       }
       case _ => false
