6a90fa8e7107120a141925859d486c6ceea18958
ilya.klyuchnikov <none@none>
2009-05-04 20:37:15 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40437

--- hosc_tests/src/hosc/InterpreterTest.scala
+++ hosc_tests/src/hosc/InterpreterTest.scala
@@ -1,29 +1,15 @@
 package hosc
 
 import org.junit.Test
-import org.junit.Ignore
 import org.junit.Assert._
-
-import HLanguage._
 import TestUtils._
 
 class InterpreterTest {
   
-  @Test def app(): Unit = {
-    testInt("hl/interpreter/app.hs", "Cons True (Cons False Nil)")
-  }
-  
-  @Test def rev(): Unit = {
-    testInt("hl/interpreter/rev.hs", "Cons False (Cons True Nil)")
-  }
-  
-  @Test def laziness(): Unit = {
-    testInt("hl/interpreter/lazy.hs", "Cons Z (Cons (S Z) Nil)")
-  }
-  
-  @Test def church(): Unit = {
-    testInt("hl/interpreter/church.hs", "(S (Z ))")
-  }
+  @Test def app() = testInt("interpreter/app.hs", "Cons True (Cons False Nil)")
+  @Test def rev() = testInt("interpreter/rev.hs", "Cons False (Cons True Nil)")
+  @Test def laziness() = testInt("interpreter/lazy.hs", "Cons Z (Cons (S Z) Nil)")
+  @Test def church() = testInt("interpreter/church.hs", "(S (Z ))")
   
   def testInt(fileName: String, expectedOutput: String): Unit = {
     val in = new Interpreter(fileName)
