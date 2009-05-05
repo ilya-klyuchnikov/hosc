508649f1bffe5f37ba58058dcb70c03611c04b85
ilya.klyuchnikov <none@none>
2009-05-05 22:24:47 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40455

--- hosc_tests/hl/supercompiler/letrec.hs
+++
@@ -1,9 +0,0 @@
-data List a = Nil  | Cons a (List a);
-// app x (app y z)
-letrec
-  f=\w1 -> case w1 of {
-    Nil -> (letrec g=\p1 -> case p1 of { Nil -> z; Cons y1 t -> (Cons y1 (g t)); } in (g y));
-    Cons s u1 -> (Cons s (f u1));
-  }
-in
-  f x
