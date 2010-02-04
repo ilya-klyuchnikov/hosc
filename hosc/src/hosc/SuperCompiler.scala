99c96014ae5bf5ec6445c0a0edd5c98c6f5c8293
ilya.klyuchnikov <none@none>
2010-02-04 08:30:27 +0000

    extracting renaming of variables into a separate trait
    --HG--
    extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40558

--- hosc/src/hosc/SuperCompiler.scala
+++ hosc/src/hosc/SuperCompiler.scala
@@ -7,7 +7,7 @@ import TermAlgebra._
 import ProcessTree._
 import LangUtils._
 
-class SuperCompiler(val program: Program) extends ASupercompiler{
+class SuperCompiler(val program: Program) extends ASupercompiler with ProcessTreeRenamer {
   val emptyMap = Map[Variable, Expression]()
   val debug = false
   val useControl = true
@@ -150,38 +150,5 @@ class SuperCompiler(val program: Program) extends ASupercompiler{
       }
       t.replace(alpha, LetExpression(g.sub1, g.term))
     }    
-  }
-  
-  def renameVars(p: ProcessTree): ProcessTree = {
-    val vars = p.rootNode.getAllVars()
-    var i = 0
-    def createVar(): Variable = {      
-      var nv: Variable = null
-      do {
-        nv = varFor(i)
-        i += 1
-      } while (vars contains nv)
-      nv
-    }
-    var map = Map[Variable, Variable]()
-    for (v <- vars.toList) {
-      if (isSynthetic(v)) {
-        map = map + (v -> createVar)
-      }
-    }
-    p.rootNode sub map
-    p
-  }
-  
-  private val vNames = "xyzuvwprst".toArray
-  
-  private def varFor(j: Int) = {
-    if (j <= 9) 
-      Variable("" + vNames(j))
-    else 
-      Variable("" + vNames(j % 10) + Integer.toString(j / 10))   
-  }
-  
-  private def isSynthetic(v: Variable) = v.name startsWith "$" 
-  
+  }  
 }
