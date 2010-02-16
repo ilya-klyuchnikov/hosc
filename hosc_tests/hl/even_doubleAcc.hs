25c84828519911f52625fcb3785f45c9ebc7d4ea
ilya.klyuchnikov <none@none>
2010-02-16 08:18:18 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40587

--- hosc_tests/hl/even_doubleAcc.hs
+++ hosc_tests/hl/even_doubleAcc.hs
@@ -1,21 +1,21 @@
-data Bool = True | False;
+data Bool = T | F;
 data Nat = Z | S Nat;
 
-even (doubleAcc x Z) where
+even (dAcc x Z) where
 
 even = \x ->
   case x of {
-    Z -> True;
+    Z -> T;
     S x1 -> odd x1;
 };
 
 odd = \x ->
   case x of {
-    Z -> False;
+    Z -> F;
     S x1 -> even x1;
 };
 
-doubleAcc = \x y -> case x of {
+dAcc = \x y -> case x of {
   Z -> y;
-  S x1 -> doubleAcc x1 (S (S y));
+  S x1 -> dAcc x1 (S (S y));
 };
