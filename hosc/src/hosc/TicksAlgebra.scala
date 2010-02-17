75a10b593ffbafb172b01a84a2087653bc30d26f
ilya.klyuchnikov <none@none>
2010-02-17 07:48:19 +0000

    --HG-- extra : convert_revision : svn%3A04d2d0ec-5548-0410-87ee-dd41c663a700/trunk%40589

--- hosc/src/hosc/TicksAlgebra.scala
+++ hosc/src/hosc/TicksAlgebra.scala
@@ -16,16 +16,14 @@ object TicksAlgebra {
         isImprovement(b1, b2)
       case (Application(h1, a1), Application(h2, a2)) =>
         isImprovement(a1, a2) && isImprovement(h1, h2)
+      case (LetRecExpression((_, b1), e1), LetRecExpression((_, b2), e2)) => 
+        isImprovement(e1, e2) && isImprovement(b1, b2)
       case (CaseExpression(sel1, bs1), CaseExpression(sel2, bs2)) => {
         val bs1_ = bs1 sort TermAlgebra.compareB
         val bs2_ = bs2 sort TermAlgebra.compareB
-        //val samePatterns = (bs1_ map (_.pattern.name)) == (bs2_ map (_.pattern.name))
-        //samePatterns && 
         isImprovement(sel1, sel2) && 
           ((bs1_ zip bs2_) forall (bs => bs._1.pattern.name == bs._2.pattern.name && isImprovement(bs._1.term, bs._2.term)))
       }
-      case (LetRecExpression((_, b1), e1), LetRecExpression((_, b2), e2)) =>
-        isImprovement(e1, e2) && isImprovement(b1, b2)
       case _ => false
     }
   }
