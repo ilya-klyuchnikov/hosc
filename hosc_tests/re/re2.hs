c4b5ad1f70980427e45a3a22c5a1f40fbb02195c
ilya.klyuchnikov <none@none>
2010-04-18 00:29:55 +0400

    next version of re

--- hosc_tests/re/re2.hs
+++ hosc_tests/re/re2.hs
@@ -3,35 +3,54 @@ data List a = Nil | Cons a (List a);
 data Bool = True | False;
 data Option a = None | Some a; 
 
-notEmpty ((or a a) w)
+--notEmpty ((or a a) w)
+
+--notEmpty ((concat a a) w)
+
+--notEmpty ((concat (rep a) eow) w)
+
+--notEmpty (a return) w
+
+--(a return) w
+
+--concat (or a b) eow return w
+
+--concat a (concat (rep a) eow) return w
+
+notEmpty (concat (rep a) (concat a eow) return w)
 
 where
 
-or = \p1 p2 w -> case p1 w of {
-	None -> p2 w;
-	Some w1 -> case p2 w of {
+concat = \p1 p2 k w -> p1 (p2 k) w;
+
+or = \p1 p2 k w -> case p1 k w of {
+	None -> p2 k w;
+	Some w1 -> case p2 k w of {
 		None -> Some w1;
 		Some w2 -> choice{Some w1; Some w2;};
 	};
 };
-
-a = \w -> case w of {
-	Nil -> None;
-	Cons l w1 -> case l of { A -> Some w1; B -> None;};
+rep = \p1 next w -> case p1 return w of {
+	None -> next w;
+	Some w1 -> case next w of {
+		None -> rep p1 next w1;
+		Some w2 -> choice {Some w; rep p1 next w1;}; 
+	};
 };
-
-b = \w -> case w of {
+a = \next w -> case w of {
 	Nil -> None;
-	Cons l w1 -> case l of {A -> None; B -> Some w1;};
+	Cons l w1 -> case l of { A -> next w1; B -> None;};
 };
-
-nilP = \w -> Some w;
-
-eow = \w -> case w of {
-	Nil -> Some Nil;
+b = \next w -> case w of {
+	Nil -> None;
+	Cons l w1 -> case l of {A -> None; B -> next w1;};
+};
+nilP = \next w -> next w;
+eow = \next w -> case w of {
+	Nil -> next Nil;
 	Cons l w1 -> None;
 };
-
+return = \y -> Some y;
 notEmpty = \o -> case o of {
 	Some x -> True;
 	None -> False;
