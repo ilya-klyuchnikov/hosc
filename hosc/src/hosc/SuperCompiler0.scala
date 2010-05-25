515008931d120c65ab7b0c6c558fd0aaf14686fa
ilya.klyuchnikov <none@none>
2010-05-25 10:49:12 +0400

    for experiments

--- hosc/src/hosc/SuperCompiler0.scala
+++ hosc/src/hosc/SuperCompiler0.scala
@@ -159,6 +159,7 @@ class SuperCompiler0(val program: Program) extends ASupercompiler with ProcessTr
     case Context(RedexCall(_)) => true
     case Context(RedexCaseVar(_, _)) => true
     case Context(RedexCaseCon(_, _)) => true
+    //case Context(RedexLamApp(lam, app)) => true
     case _ => false
   }
   
