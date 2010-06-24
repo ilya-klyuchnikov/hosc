83b8b817f122ee056613f01ee67f8ec458a16dc3
ilya.klyuchnikov <none@none>
2010-06-24 07:58:22 +0400

    more experiments
    --HG--
    rename : hosc/src/hosc/exp/HLSC1.scala => hosc/src/hosc/exp/FastHLSC.scala

--- hosc/src/hosc/exp/FastHLSC.scala
+++ hosc/src/hosc/exp/FastHLSC.scala
@@ -8,21 +8,32 @@ import hosc.ProcessTree._
 import hosc.TermAlgebra
 import hosc.lemmas.LemmaFinder
 
-class HLSC1(program: Program) extends SuperCompiler0(program){
+// Fast Higher-Level Supercompiler
+// the source for lemmas is computation history.
+class FastHLSC(program: Program) extends SuperCompiler0(program){
   val finder = new LemmaFinder(program)
   debug = false
   
   override def abstractUp(t: ProcessTree, up: Node, down: Node): Unit = {
-    parents(down) find {good(down)} match {
-      case None => {
+    println("=======")
+    println(up.expr)
+    println(down.expr)
+    parents(down) map {good(down)} filter{_.isDefined} match {
+      case Nil => {
         assume(false, "None")
         //super.abstractUp(t, up, down)
         println("passing")
         drive(t, down)
       }
-      case Some(u) => {
-        assume(false, "Some")
+      case Some(u) :: _ => {
+        println(u)
+        //assume(false, "Some")
         println("********")
+        println("replacing")
+        println(down.expr)
+        println("=>")
+        println(u)
+        t.replace(down, u)
       }
     }
   } 
@@ -42,14 +53,26 @@ class HLSC1(program: Program) extends SuperCompiler0(program){
     new CodeConstructor(program, pt, true).generateProgram().goal
   }
   
-  def good(down: Node)(cand: Node): Boolean = {
+  def good(down: Node)(cand: Node): Option[Expression] = {
     cand.expr match {
-    case l:LetExpression => false
+    case l:LetExpression => None
     case e => {
       val e0 = sc(down.expr)
       val e1 = sc(cand.expr)
-      return TermAlgebra.equivalent(e0, e1)
+      if (TermAlgebra.equivalent(e0, e1)) {
+        //println(cand.expr)
+        //println(down.expr)
+        //println(e1)
+        //println(e0)
+        println(TicksAlgebra.isImprovement(e1, e0))
+        val sub = Instance.findSubst(e1, e0)
+        val res = TermAlgebra.applySubstitution(cand.expr, sub)
+        //println(res)
+        Some(res)
+      } else {
+        None
+      }
     }
   }
-    }
+  }
 }
