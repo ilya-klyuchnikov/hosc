1d6ade0f71fb9335fe710a2173ec00570f47291a
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2010-08-03 10:15:24 +0400

    towards many supercompilers (for comparing)

---
+++ src/main/scala/hosc/sc/Driver.scala
@@ -0,0 +1,74 @@
+package hosc.sc
+
+import hosc.HLanguage._
+import hosc.ProcessTree
+import hosc.ProcessTree._
+import hosc.TermAlgebra._
+
+// the basic driver
+// it doesn't support propagation of missing pattern
+// upward the process tree - just replaces 
+// con<case C of {...}> => case C of {} 
+trait Driver {
+  def program: Program
+
+  def driveNode(tree: ProcessTree, node: Node): ProcessTree = node.expr match {
+
+    case LetExpression(bs, t) => {
+      val childExprs = t :: (bs map { case (v, e) => e })
+      tree.addChildren(node, childExprs)
+    }
+
+    case expr => decompose(expr) match {
+
+      case ObservableCon(ctr) if !ctr.args.isEmpty => {
+        val childExprs = ctr.args
+        tree.addChildren(node, childExprs)
+      }
+
+      case ObservableVarApp(_, app) => {
+        val childExprs = extractAppArgs(app)
+        tree.addChildren(node, childExprs)
+      }
+
+      case ObservableLam(lam) => {
+        val childExprs = List(lam.t)
+        tree.addChildren(node, childExprs)
+      }
+
+      case context@Context(RedexCall(v)) => {
+        val lam = program.getFunction(v.name).get.body
+        val childExprs = List(context.replaceHole(freshBinders(lam)))
+        tree.addChildren(node, childExprs)
+      }
+
+      case context@Context(RedexCaseCon(c, ce)) => {
+        val branch = ce.branches.find(_.pattern.name == c.name).get
+        val sub = Map(branch.pattern.args zip c.args: _*)
+        val childExprs = List(freshBinders(context.replaceHole(applySubstitution(branch.term, sub))))
+        tree.addChildren(node, childExprs)
+      }
+
+      case context@Context(RedexLamApp(lam, app)) => {
+        val child = freshBinders(context.replaceHole(applySubstitution(lam.t, Map(lam.v -> app.arg))))
+        tree.addChildren(node, List(child))
+      }
+
+      // TODO: make full information propagation here:
+      // 1) make "promised" propagation
+      case context@Context(RedexCaseVar(_, CaseExpression(sel, bs))) => {
+        val childForSel = freshBinders(sel)
+        val branchTransform = { b: Branch =>
+          replaceTerm(context.replaceHole(b.term), sel, Constructor(b.pattern.name, b.pattern.args))
+        }
+        val childrenForBranches = bs map { b => freshBinders(branchTransform(b)) }
+        val childExprs = childForSel :: childrenForBranches
+        tree.addChildren(node, childExprs)
+      }
+
+      case _ => throw new IllegalArgumentException("non reducible expression: " + expr)
+
+    }
+  }
+
+}
--- src/main/scala/hosc/sc_old/SuperCompilers.scala
+++ src/main/scala/hosc/sc_old/SuperCompilers.scala
@@ -2,6 +2,7 @@ package hosc.sc_old
 
 import hosc.HLanguage._
 import hosc.ProcessTree._
+import hosc.sc.Driver
 
 class SuperCompiler(val program: Program) extends SuperCompilerAlgorithm with Driver with TreeOperations {
   val renaming  = RenamingOrdering
