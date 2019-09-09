bfe8447a9a44a812d048a47b2da0a319a770c665
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2019-09-09 10:35:41 +0100

    types: HLanguages: simplifications

--- src/main/scala/hosc/HLanguage.scala
+++ src/main/scala/hosc/HLanguage.scala
@@ -10,40 +10,24 @@ import scala.text.Document._
 object HLanguage {
    val ED: scala.text.Document = empty
    sealed abstract class Expression extends Positional {
-     var ticks = 0
-     def incrTicks():Expression = {
-       ticks += 1
-       this
-     }
-     def ticksStr(): String = {
-       var res = ""
-       for (i <- 1 to ticks) {
-         res += "+"
-       }
-       res
-     }
      def toDoc: Document
      type termType <: Expression
      def \\(s: Map[Variable, Variable]): termType
      def / (s: Map[Variable, Expression]): Expression
-     def copyTicks(that: Expression): Expression = {
-       that.ticks = ticks
-       that
-     }
    }
    case class Variable(name: String) extends Expression {
      type termType = Variable
      def  \\(s: Map[Variable, Variable]) = s.get(this) match {case Some(t) => t; case None => this}
-     def / (s: Map[Variable, Expression]) = s.get(this) match {case Some(t) => copyTicks(t); case None => this}
+     def / (s: Map[Variable, Expression]) = s.get(this) match {case Some(t) => t; case None => this}
      var global = false // global var is call
-     override def toString = ticksStr + name
+     override def toString = name
      def toDoc = text(name)
    } 
    case class Constructor(name: String, args: List[Expression]) extends Expression {
      type termType = Constructor
      def \\(s: Map[Variable, Variable]) = Constructor(name, args map {_\\s})
-     def / (s: Map[Variable, Expression]) = copyTicks(Constructor(name, args map {_/s}))
-     override def toString = ticksStr + "(" + name + (args match {case Nil => ""; case _ => args.mkString(" ", " ","")}) + ")"
+     def / (s: Map[Variable, Expression]) = Constructor(name, args map {_/s})
+     override def toString = "(" + name + (args match {case Nil => ""; case _ => args.mkString(" ", " ","")}) + ")"
      def toDoc = args match {
        case Nil => text(name)
        case _ => group(("(" + name) :: nest(2, ED :: args.foldRight(ED){(x, y) => ED :/: x.toDoc :: y}) :: ")" :: ED)
@@ -52,31 +36,30 @@ object HLanguage {
    case class LambdaAbstraction(v: Variable, t: Expression) extends Expression {
      type termType = LambdaAbstraction
      def \\(s: Map[Variable, Variable]) = LambdaAbstraction(v\\s, t\\s)
-     def / (s: Map[Variable, Expression]) = copyTicks(LambdaAbstraction(v, t/s))
-     override def toString = ticksStr + "(\\" + v.name + " -> " + t + ")"
+     def / (s: Map[Variable, Expression]) = LambdaAbstraction(v, t/s)
+     override def toString = "(\\" + v.name + " -> " + t + ")"
      def toDoc = "(\\" :: v.toDoc :: "->" :: nest(2, ED :/: t.toDoc) :: ")" :: ED 
    }
    case class Application(head: Expression, arg: Expression) extends Expression {
      type termType = Application
      def \\(s: Map[Variable, Variable]) = Application(head\\s, arg\\s)
-     def / (s: Map[Variable, Expression]) = copyTicks(Application(head/s, arg/s))
+     def / (s: Map[Variable, Expression]) = Application(head/s, arg/s)
      pos = head.pos
-     override def toString = ticksStr + "(" + head + " " + arg + ")"
+     override def toString = "(" + head + " " + arg + ")"
      def toDoc = group("(" :: nest(2, head.toDoc :/: arg.toDoc ) :: ")" :: ED)
    } 
    case class CaseExpression(selector: Expression, branches: List[Branch]) extends Expression {
      type termType = CaseExpression
      def \\(s: Map[Variable, Variable]) = CaseExpression(selector\\s, branches map {_\\s})
-     def / (s: Map[Variable, Expression]) = copyTicks(CaseExpression(selector/s, branches map {b => Branch(b.pattern, b.term / s)}))
-     override def toString = ticksStr + "case (" + selector + ") of " + branches.mkString("{", " ", "}")
+     def / (s: Map[Variable, Expression]) = (CaseExpression(selector/s, branches map {b => Branch(b.pattern, b.term / s)}))
+     override def toString = "case (" + selector + ") of " + branches.mkString("{", " ", "}")
      def toDoc = group( group("case " :/: selector.toDoc :/: " of {" :: ED) :: 
        nest(2, branches.foldRight(ED){(b, y) => ED :/: b.toDoc :: y}) :/: "}" :: ED)
    }
    case class LetExpression(bs: List[(Variable, Expression)], expr: Expression) extends Expression {
      type termType = LetExpression
-     // we need asInstanceOf due to http://lampsvn.epfl.ch/trac/scala/ticket/252
-     def \\(s: Map[Variable, Variable]) = LetExpression(bs map {b => ((b._1\\s).asInstanceOf[Variable], (b._2\\s).asInstanceOf[Expression])}, expr\\s);
-     def / (s: Map[Variable, Expression]) = LetExpression(bs map {b => (b._1, (b._2/s).asInstanceOf[Expression])}, expr/s)
+     def \\(s: Map[Variable, Variable]) = LetExpression(bs map {b => ((b._1\\s), (b._2\\s).asInstanceOf[Expression])}, expr\\s);
+     def / (s: Map[Variable, Expression]) = LetExpression(bs map {b => (b._1, (b._2/s))}, expr/s)
      override def toString = "let " + (bs map {p => p._1 + "=" + p._2}).mkString(", ") + "\n in " + expr 
      def toDoc = group("let" :: 
            nest(2, bs.foldLeft(ED){(y, b) => y :/: group (b._1.toDoc :: " = " :: b._2.toDoc)})
@@ -85,8 +68,8 @@ object HLanguage {
    case class LetRecExpression(binding: (Variable, Expression), expr: Expression) extends Expression {
      type termType = LetRecExpression
      def \\(s: Map[Variable, Variable]) = LetRecExpression((binding._1\\s, binding._2\\s), expr\\s);
-     def / (s: Map[Variable, Expression]) = copyTicks(LetRecExpression((binding._1, binding._2/s), expr/s))
-     override def toString = ticksStr + "(letrec " + (binding._1 + "=" + binding._2) + "\n in " + expr + ")"
+     def / (s: Map[Variable, Expression]) = LetRecExpression((binding._1, binding._2/s), expr/s)
+     override def toString = "(letrec " + (binding._1 + "=" + binding._2) + "\n in " + expr + ")"
      def toDoc = group("(letrec" :: 
          nest(2, group (ED :/: binding._1.toDoc :: "=" :: binding._2.toDoc))
          :/: "in" :: nest(2, ED :/: expr.toDoc) :: ")" :: ED)
