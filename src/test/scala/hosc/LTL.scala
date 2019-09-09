d265d0910de7a7c47d1a2fcd8940cd097479576a
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2019-09-09 10:25:30 +0100

    types: minimizing tests

--- src/test/scala/hosc/LTL.scala
+++
@@ -1,9 +0,0 @@
-package hosc
-
-import hosc.exp.{SuperCompilerApp1 => SCP1, SuperCompilerApp2 => SCP2}
-
-object LTL {
-	def main(args : Array[String]) : Unit = {
-	   SCLemApp.main(Array("-si", "examples/mc/LTL1.hs", "-li", "examples/mc/lemmas.hs", "-t", "out0/LTL1.svg", "-p", "out0/LTL1.hs"))
-	}
-}
