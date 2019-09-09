5068f182c9535f3069dc007c9ef95564fc3469cd
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2019-09-09 12:20:31 +0100

    types: HLanguage cleanup

--- src/main/scala/hosc/HLanguage.scala
+++ src/main/scala/hosc/HLanguage.scala
@@ -17,77 +17,108 @@ object HLanguage {
    }
    case class Variable(name: String) extends Expression {
      type termType = Variable
-     def  \\(s: Map[Variable, Variable]) = s.get(this) match {case Some(t) => t; case None => this}
-     def / (s: Map[Variable, Expression]) = s.get(this) match {case Some(t) => t; case None => this}
+     def  \\(s: Map[Variable, Variable]): Variable =
+       s.get(this) match {case Some(t) => t; case None => this}
+     def / (s: Map[Variable, Expression]): Expression =
+       s.get(this) match {case Some(t) => t; case None => this}
      var global = false // global var is call
-     override def toString = name
-     def toDoc = text(name)
+     override def toString: String =
+       name
+     def toDoc: Document =
+       text(name)
    } 
    case class Constructor(name: String, args: List[Expression]) extends Expression {
      type termType = Constructor
-     def \\(s: Map[Variable, Variable]) = Constructor(name, args map {_\\s})
-     def / (s: Map[Variable, Expression]) = Constructor(name, args map {_/s})
-     override def toString = "(" + name + (args match {case Nil => ""; case _ => args.mkString(" ", " ","")}) + ")"
-     def toDoc = args match {
-       case Nil => text(name)
-       case _ => group(("(" + name) :: nest(2, ED :: args.foldRight(ED){(x, y) => ED :/: x.toDoc :: y}) :: ")" :: ED)
-     }  
+     def \\(s: Map[Variable, Variable]): Constructor =
+       Constructor(name, args map {_\\s})
+     def / (s: Map[Variable, Expression]): Expression =
+       Constructor(name, args map {_/s})
+     override def toString: String =
+       "(" + name + (args match {case Nil => ""; case _ => args.mkString(" ", " ","")}) + ")"
+     def toDoc: Document =
+       args match {
+        case Nil => text(name)
+        case _ => group(("(" + name) :: nest(2, ED :: args.foldRight(ED){(x, y) => ED :/: x.toDoc :: y}) :: ")" :: ED)
+      }
    }
    case class LambdaAbstraction(v: Variable, t: Expression) extends Expression {
      type termType = LambdaAbstraction
-     def \\(s: Map[Variable, Variable]) = LambdaAbstraction(v\\s, t\\s)
-     def / (s: Map[Variable, Expression]) = LambdaAbstraction(v, t/s)
-     override def toString = "(\\" + v.name + " -> " + t + ")"
-     def toDoc = "(\\" :: v.toDoc :: "->" :: nest(2, ED :/: t.toDoc) :: ")" :: ED 
+     def \\(s: Map[Variable, Variable]): LambdaAbstraction =
+       LambdaAbstraction(v\\s, t\\s)
+     def / (s: Map[Variable, Expression]): Expression =
+       LambdaAbstraction(v, t/s)
+     override def toString: String =
+       "(\\" + v.name + " -> " + t + ")"
+     def toDoc: Document =
+       "(\\" :: v.toDoc :: "->" :: nest(2, ED :/: t.toDoc) :: ")" :: ED
    }
    case class Application(head: Expression, arg: Expression) extends Expression {
      type termType = Application
-     def \\(s: Map[Variable, Variable]) = Application(head\\s, arg\\s)
+     def \\(s: Map[Variable, Variable]): Application =
+       Application(head\\s, arg\\s)
      def / (s: Map[Variable, Expression]) = Application(head/s, arg/s)
      pos = head.pos
-     override def toString = "(" + head + " " + arg + ")"
-     def toDoc = group("(" :: nest(2, head.toDoc :/: arg.toDoc ) :: ")" :: ED)
+     override def toString: String =
+       "(" + head + " " + arg + ")"
+     def toDoc: Document =
+       group("(" :: nest(2, head.toDoc :/: arg.toDoc ) :: ")" :: ED)
    } 
    case class CaseExpression(selector: Expression, branches: List[Branch]) extends Expression {
      type termType = CaseExpression
-     def \\(s: Map[Variable, Variable]) = CaseExpression(selector\\s, branches map {_\\s})
-     def / (s: Map[Variable, Expression]) = (CaseExpression(selector/s, branches map {b => Branch(b.pattern, b.term / s)}))
-     override def toString = "case (" + selector + ") of " + branches.mkString("{", " ", "}")
-     def toDoc = group( group("case " :/: selector.toDoc :/: " of {" :: ED) :: 
-       nest(2, branches.foldRight(ED){(b, y) => ED :/: b.toDoc :: y}) :/: "}" :: ED)
+     def \\(s: Map[Variable, Variable]): CaseExpression =
+       CaseExpression(selector\\s, branches map {_\\s})
+     def / (s: Map[Variable, Expression]): Expression =
+       CaseExpression(selector/s, branches map { b => Branch(b.pattern, b.term / s)})
+     override def toString: String =
+       "case (" + selector + ") of " + branches.mkString("{", " ", "}")
+     def toDoc: Document =
+       group( group("case " :/: selector.toDoc :/: " of {" :: ED) :: nest(2, branches.foldRight(ED){(b, y) => ED :/: b.toDoc :: y}) :/: "}" :: ED)
    }
    case class LetExpression(bs: List[(Variable, Expression)], expr: Expression) extends Expression {
      type termType = LetExpression
-     def \\(s: Map[Variable, Variable]) = LetExpression(bs map {b => ((b._1\\s), (b._2\\s).asInstanceOf[Expression])}, expr\\s);
-     def / (s: Map[Variable, Expression]) = LetExpression(bs map {b => (b._1, (b._2/s))}, expr/s)
-     override def toString = "let " + (bs map {p => p._1 + "=" + p._2}).mkString(", ") + "\n in " + expr 
-     def toDoc = group("let" :: 
+     def \\(s: Map[Variable, Variable]): LetExpression =
+       LetExpression(bs map { b => (b._1\\s, (b._2\\s).asInstanceOf[Expression])}, expr\\s)
+     def / (s: Map[Variable, Expression]): Expression =
+       LetExpression(bs map { b => (b._1, b._2/s)}, expr/s)
+     override def toString: String =
+       "let " + (bs map { p => p._1 + "=" + p._2}).mkString(", ") + "\n in " + expr
+     def toDoc: Document =
+       group("let" ::
            nest(2, bs.foldLeft(ED){(y, b) => y :/: group (b._1.toDoc :: " = " :: b._2.toDoc)})
            :/: "in " :: nest(2, ED :/: expr.toDoc))
    }
    case class LetRecExpression(binding: (Variable, Expression), expr: Expression) extends Expression {
      type termType = LetRecExpression
-     def \\(s: Map[Variable, Variable]) = LetRecExpression((binding._1\\s, binding._2\\s), expr\\s);
-     def / (s: Map[Variable, Expression]) = LetRecExpression((binding._1, binding._2/s), expr/s)
-     override def toString = "(letrec " + (binding._1 + "=" + binding._2) + "\n in " + expr + ")"
-     def toDoc = group("(letrec" :: 
+     def \\(s: Map[Variable, Variable]): LetRecExpression =
+       LetRecExpression((binding._1\\s, binding._2\\s), expr\\s)
+     def / (s: Map[Variable, Expression]): Expression =
+       LetRecExpression((binding._1, binding._2/s), expr/s)
+     override def toString: String =
+       "(letrec " + (binding._1 + "=" + binding._2) + "\n in " + expr + ")"
+     def toDoc: Document =
+       group("(letrec" ::
          nest(2, group (ED :/: binding._1.toDoc :: "=" :: binding._2.toDoc))
          :/: "in" :: nest(2, ED :/: expr.toDoc) :: ")" :: ED)
    }
    
    case class Branch(pattern: Pattern, term: Expression) extends Positional {
-     def \\(s: Map[Variable, Variable]) = Branch(pattern\\s, term\\s)
-     override def toString = pattern + " -> " + term + ";"
-     def toDoc: Document = group(pattern.toDoc :: " ->" :: nest(2 , ED :/: term.toDoc :: ";" :: ED)); 
+     def \\(s: Map[Variable, Variable]): Branch = Branch(pattern\\s, term\\s)
+     override def toString: String =
+       pattern + " -> " + term + ";"
+     def toDoc: Document =
+       group(pattern.toDoc :: " ->" :: nest(2 , ED :/: term.toDoc :: ";" :: ED))
    }
    case class Pattern(name: String, args: List[Variable]) extends Positional {
      def \\(s: Map[Variable, Variable]) = Pattern(name, args map {_\\s})
-     override def toString = name + " " + args.mkString(" ")
-     def toDoc = text(toString)
+     override def toString: String =
+       name + " " + args.mkString(" ")
+     def toDoc: Document =
+       text(toString)
    }
    
    case class Function(name: String, body: Expression) extends Positional {
-     def toDoc = text(name) :: text(" = ") :: body.toDoc :: ";" :: ED
+     def toDoc: Document =
+       text(name) :: text(" = ") :: body.toDoc :: ";" :: ED
    }
    
    case class Program(ts: List[TypeConstructorDefinition], goal: Expression, fs: List[Function]) {
@@ -111,11 +142,11 @@ object HLanguage {
        for (f <- fs) if (f.name == n) return Some(f)
        None
      }
-     def toDoc = (ED /: ts) {(doc, td) => doc :/: nest(0, text(td.toString))} :/: 
+     def toDoc: Document = (ED /: ts) { (doc, td) => doc :/: nest(0, text(td.toString))} :/:
                  ED :/: nest(0, goal.toDoc) :/: 
                  (if (fs.isEmpty) ED else text("where") :/: (ED /: fs) {(doc, f) => doc :/: nest(0, f.toDoc)})
      
-     def toDocString = {
+     def toDocString: String = {
        val doc1 = toDoc    
        val writer1 = new java.io.StringWriter()
        doc1.format(120, writer1)
@@ -127,17 +158,19 @@ object HLanguage {
 
 sealed abstract class Type extends Positional
 case class TypeVariable(name: String) extends Type {
-  override def toString = name
+  override def toString: String =
+    name
 }
 case class TypeConstructor(name: String, typeParameters: List[Type]) extends Type {
-  override def toString = typeParameters match {
+  override def toString: String = typeParameters match {
     case Nil => name
     case _   => "(" + name + " " + typeParameters.mkString(" ") + ")";
   }
 }
 case class Arrow(t1: Type, t2: Type) extends Type {
   pos = t1.pos
-  override def toString = "(" + t1 + "->" + t2 + ")"
+  override def toString: String =
+    "(" + t1 + "->" + t2 + ")"
 }
  
 abstract sealed class TypeDefinition extends Positional {
@@ -145,8 +178,10 @@ abstract sealed class TypeDefinition extends Positional {
 }
 case class TypeConstructorDefinition(name: String, args: List[TypeVariable], cons: List[DataConstructor])
   extends TypeDefinition {
-  override def toString = "data " + name + " " + args.mkString(" ") + " = " + cons.mkString(" | ") + ";";     
+  override def toString: String =
+    "data " + name + " " + args.mkString(" ") + " = " + cons.mkString(" | ") + ";"
 }
 case class DataConstructor(name: String, args: List[Type]) extends Positional {
-  override def toString = name + " " + args.mkString(" ")
+  override def toString: String =
+    name + " " + args.mkString(" ")
 }
