3e6090198106c6886fec433a4755524281844c7d
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-11-17 00:17:34 +0000

    simpler

--- src/main/scala/hosc/Util.scala
+++ src/main/scala/hosc/Util.scala
@@ -1,9 +1,11 @@
 package hosc;
 
-import scala.util.parsing.input.{CharArrayReader, StreamReader}
 import java.io.{File, FileReader}
-import HLanguage._
-import LangUtils._
+
+import hosc.HLanguage._
+import hosc.LangUtils._
+
+import scala.util.parsing.input.StreamReader
 
 object Util {
   def programFromFile(fileName: String): Program = {
@@ -20,18 +22,6 @@ object Util {
     }
   }
 
-  def programFromString(input: String): Program = {
-    val pr = HParsers.parseProgram(new CharArrayReader(input.toCharArray))
-    if (pr.successful) {
-      val program = pr.get
-      val ti = new TypeInferrer(program.ts)
-      ti.inferType(normalize(program))
-      program
-    } else {
-      throw new IllegalArgumentException(pr.toString)
-    }
-  }
-
   def inferGoalType(fileName: String): Type = {
     val in = new FileReader(new File(fileName))
     val pr = HParsers.parseProgram(StreamReader(in))
@@ -40,16 +30,6 @@ object Util {
       val program1 = LambdaLifting.lift(program)
       val ti = new TypeInferrer(program1.ts)
       ti.inferType(normalize(program1))
-    } else {
-      throw new IllegalArgumentException(pr.toString)
-    }
-  }
-
-  def rawProgramFromFile(fileName: String): Program = {
-    val in = new FileReader(new File(fileName))
-    val pr = HParsers.parseProgram(StreamReader(in))
-    if (pr.successful) {
-      pr.get
     } else {
       throw new IllegalArgumentException(pr.toString)
     }
