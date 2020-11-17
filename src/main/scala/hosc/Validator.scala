2c78f946965f2505943e9dea64016c845421958d
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-11-17 00:16:12 +0000

    simpler

--- src/main/scala/hosc/Validator.scala
+++ src/main/scala/hosc/Validator.scala
@@ -11,31 +11,29 @@ object Validator {
     val p = s.get
     var typeNames = Set[String]()
     var dataConNames = Set[String]()
-    var fNames = Set[String]()
+    val fNames = Set[String]()
     val fNamesInDefs = Set.empty[String] ++ (p.fs map (_.name))
 
-    def valTD(td: TypeDefinition) = {
+    def valTD(td: TypeDefinition): Unit = {
       var tvs = Set.empty[TypeVariable]
       var tvsUsed = Set.empty[TypeVariable]
 
       def valTypeUsage(t: Type, allowFreeVars: Boolean): Unit = t match {
-        case tv: TypeVariable => {
+        case tv: TypeVariable =>
           if (!allowFreeVars && !(tvs contains tv)) err("undefined var " + tv.name)
           tvsUsed += tv
-        }
         case Arrow(t1, t2) => valTypeUsage(t1, allowFreeVars); valTypeUsage(t2, allowFreeVars);
-        case tc @ TypeConstructor(n, args) => {
+        case TypeConstructor(n, args) =>
           p.getTypeConstructorDefinition(n) match {
-            case Some(td) => {
+            case Some(td) =>
               if (td.args.length != args.length) err("wrong numbers of argument for type " + n)
               for (a <- args) valTypeUsage(a, allowFreeVars)
-            }
-            case None => err("unknown type " + n)
+            case None =>
+              err("unknown type " + n)
           }
-        }
       }
 
-      def valDC(dc: DataConstructor) = {
+      def valDC(dc: DataConstructor): Unit = {
         if (dataConNames contains dc.name) err("duplicate data constructor definition " + dc.name)
         dataConNames += dc.name
         for (a <- dc.args) valTypeUsage(a, false)
@@ -45,7 +43,7 @@ object Validator {
       typeNames += td.name
 
       td match {
-        case tcd: TypeConstructorDefinition => {
+        case tcd: TypeConstructorDefinition =>
           for (v <- tcd.args) {
             if (tvs contains v) err("duplicate type variable " + v.name)
             tvs += v
@@ -55,11 +53,10 @@ object Validator {
             case Some(tv) => err("useless type variable " + tv.name)
             case None =>
           }
-        }
       }
     }
 
-    def valFD(f: Function) = {
+    def valFD(f: Function): Unit = {
       if (fNames contains f.name) err("duplicate function " + f.name, f)
       valTerm(fNamesInDefs, f.body, p)
     }
@@ -85,23 +82,25 @@ object Validator {
   def valTerm(boundedVars: Set[String], term: Expression, p: Program): Unit = term match{
     case v: Variable =>
       if (!(boundedVars contains v.name)) err("unbounded variable " + v.name, v)
-    case c: Constructor => {
+    case c: Constructor =>
       p.getDataConstructor(c.name) match {
-        case Some(dc) => {
+        case Some(dc) =>
           if (dc.args.length != c.args.length) err("wrong number of parameters for constructor " + c.name, c)
           for (arg <- c.args) valTerm(boundedVars, arg, p)
-        }
         case None => err("undefined constructor " + c.name, c)
       }
-    }
-    case l: LambdaAbstraction => {
+    case l: LambdaAbstraction =>
       if (boundedVars contains l.v.name) err("variable " + l.v.name + " is already bound", l.v)
       valTerm(boundedVars + l.v.name, l.t, p)
-    }
-    case a: Application => {valTerm(boundedVars, a.head, p); valTerm(boundedVars, a.arg, p);}
-    case c: CaseExpression => valCase(boundedVars, c, p)
-    case let: LetExpression => err("unexpected let", let)
-    case letrec: LetRecExpression => err("unexpected letrec", letrec)
+    case a: Application =>
+      valTerm(boundedVars, a.head, p)
+      valTerm(boundedVars, a.arg, p)
+    case c: CaseExpression =>
+      valCase(boundedVars, c, p)
+    case let: LetExpression =>
+      err("unexpected let", let)
+    case letrec: LetRecExpression =>
+      err("unexpected letrec", letrec)
   }
 
   private def valCase(boundedVars: Set[String], c: CaseExpression, p: Program): Unit = {
@@ -113,7 +112,7 @@ object Validator {
     val dcn = pat.name
     p.getTypeDefinitionForDC(dcn) match {
       case None => err("undefined constructor " + dcn, pat)
-      case Some(td) => {
+      case Some(td) =>
         val consNames = Set.empty[String] ++ (td.cons map (_.name))
         var usedNames = Set.empty[String]
         for (b <- c.branches){
@@ -133,34 +132,28 @@ object Validator {
         }
         //val unused = consNames -- usedNames
         //if (!(unused isEmpty)) err("case is not exhaustive. missing pattern(s) " + unused.mkString(", "), c.selector)
-      }
     }
   }
 
   def valTermWithFreeVars(boundedVars: Set[String], term: Expression, p: Program): Unit = term match{
     case v: Variable =>
-    case c: Constructor => {
+    case c: Constructor =>
       p.getDataConstructor(c.name) match {
-        case Some(dc) => {
+        case Some(dc) =>
           if (dc.args.length != c.args.length) err("wrong number of parameters for constructor " + c.name, c)
           for (arg <- c.args) valTermWithFreeVars(boundedVars, arg, p)
-        }
         case None => err("undefined constructor " + c.name, c)
       }
-    }
-    case l: LambdaAbstraction => {
+    case l: LambdaAbstraction =>
       if (boundedVars contains l.v.name) err("variable " + l.v.name + " is already bound", l.v)
       valTermWithFreeVars(boundedVars + l.v.name, l.t, p)
-    }
-    case a: Application => {
+    case a: Application =>
       valTermWithFreeVars(boundedVars, a.head, p);
       valTermWithFreeVars(boundedVars, a.arg, p);
-    }
     case c: CaseExpression => valCaseWithFreeVars(boundedVars, c, p)
-    case LetRecExpression((v, e), e0) => {
+    case LetRecExpression((v, e), e0) =>
       valTermWithFreeVars(boundedVars, e, p);
       valTermWithFreeVars(boundedVars, e0, p)
-    }
     case let: LetExpression => err("unexpected let-expression", let)
   }
 
@@ -173,7 +166,7 @@ object Validator {
     val dcn = pat.name
     p.getTypeDefinitionForDC(dcn) match {
       case None => err("undefined constructor " + dcn, pat)
-      case Some(td) => {
+      case Some(td) =>
         val consNames = Set.empty[String] ++ (td.cons map (_.name))
         var usedNames = Set.empty[String]
         for (b <- c.branches){
@@ -194,7 +187,6 @@ object Validator {
         //val unused = consNames -- usedNames
         // TODO: warning
         //if (!(unused isEmpty)) err("case is not exhaustive. missing pattern(s) " + unused.mkString(", "), c.selector)
-      }
     }
   }
 
