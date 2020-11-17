c14afe491e5d26534f9ce35fec959651e5be46d5
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2020-11-17 00:19:24 +0000

    simpler

--- src/main/scala/hosc/TermAlgebra.scala
+++ src/main/scala/hosc/TermAlgebra.scala
@@ -4,19 +4,21 @@ import HLanguage._
 
 object TermAlgebra {
   def getFreeVars(t: Expression): List[Variable] = t match {
-    case v: Variable => if (v.global) List() else List(v)
-    case Constructor(_, args) => (List[Variable]() /: args) {(vs, exp) =>  vs ++ (getFreeVars(exp).filterNot(vs.contains))}
-    case LambdaAbstraction(x, term) => getFreeVars(term).filterNot(_ == x)
-    case Application(head, arg) => {
+    case v: Variable =>
+      if (v.global) List() else List(v)
+    case Constructor(_, args) =>
+      (List[Variable]() /: args) {(vs, exp) =>  vs ++ (getFreeVars(exp).filterNot(vs.contains))}
+    case LambdaAbstraction(x, term) =>
+      getFreeVars(term).filterNot(_ == x)
+    case Application(head, arg) =>
       val headVars = getFreeVars(head)
       headVars ++ (getFreeVars(arg).filterNot(headVars.contains))
-    }
     case CaseExpression(sel, bs) =>
        (getFreeVars(sel) /: bs) {(vs, b) => vs ++ (getFreeVars(b.term).filterNot(b.pattern.args.contains).filterNot(vs.contains))}
-    case LetRecExpression((f, e), e0) => {
+    case LetRecExpression((f, e), e0) =>
       val eVars = getFreeVars(e)
       (eVars ++ (getFreeVars(e0).filterNot(eVars.contains))).filterNot(_ == f)
-    }
-    case let: LetExpression => throw new IllegalArgumentException("unexpected expr: " + let)
+    case let: LetExpression =>
+      throw new IllegalArgumentException("unexpected expr: " + let)
   }
 }
