fee511c8654c6c6a776ab855010e4f2cb556f62f
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2021-03-13 11:03:43 +0000

    reworking langutils

--- src/main/scala/hosc/LangUtils.scala
+++ src/main/scala/hosc/LangUtils.scala
@@ -2,56 +2,71 @@ package hosc;
 
 import EnrichedLambdaCalculus._
 import GraphAnalysis._
-import HLanguage.{Application => Application0, Variable => Variable0, CaseExpression => CaseExpression0,
-  Branch => Branch0, Pattern => Pattern0, Function=>Function0,
-  Constructor => Constructor0, LambdaAbstraction => LambdaAbstraction0,
-  LetExpression => LetExpression0, LetRecExpression => LetRecExpression0, Expression => Expression0, Program => Program0}
-
-import scala.collection.mutable.ListBuffer
+import HLanguage.{
+  Application => Application0,
+  Variable => Variable0,
+  CaseExpression => CaseExpression0,
+  Function => Function0,
+  Constructor => Constructor0,
+  LambdaAbstraction => LambdaAbstraction0,
+  LetExpression => LetExpression0,
+  LetRecExpression => LetRecExpression0,
+  Expression => Expression0,
+  Program => Program0
+}
 
 object LangUtils {
   def hl0ToELC(expr: Expression0): Expression = expr match {
-    case Variable0(n) => Variable(n)
-    case Constructor0(n, args) => Constructor(n, args map hl0ToELC)
-    case LambdaAbstraction0(v, e) => LambdaAbstraction(Variable(v.name), hl0ToELC(e))
-    case Application0(h, a) => Application(hl0ToELC(h), hl0ToELC(a))
+    case Variable0(n) =>
+      Variable(n)
+    case Constructor0(n, args) =>
+      Constructor(n, args map hl0ToELC)
+    case LambdaAbstraction0(v, e) =>
+      LambdaAbstraction(Variable(v.name), hl0ToELC(e))
+    case Application0(h, a) =>
+      Application(hl0ToELC(h), hl0ToELC(a))
     case CaseExpression0(sel, bs) =>
-      CaseExpression(hl0ToELC(sel),
-          bs map {b => Branch(Pattern(b.pattern.name, b.pattern.args map {v => Variable(v.name)}), hl0ToELC(b.term))})
+      CaseExpression(
+          hl0ToELC(sel),
+          bs.map(b => Branch(Pattern(b.pattern.name, b.pattern.args.map(v => Variable(v.name))), hl0ToELC(b.term)))
+      )
     case LetExpression0(bs, lexpr) =>
-      LetExpression(bs map {b => (Variable(b._1.name), hl0ToELC(b._2))}, hl0ToELC(lexpr))
+      LetExpression(bs.map(b => (Variable(b._1.name), hl0ToELC(b._2))), hl0ToELC(lexpr))
     case LetRecExpression0(b, lexpr) =>
       LetRecExpression((Variable(b._1.name), hl0ToELC(b._2)) :: Nil, hl0ToELC(lexpr))
   }
 
   def normalize(p: Program0): Expression = {
-    val fs = (Map[String, Function0]() /: p.fs) {(m, f) => m + (f.name -> f)}
-    val vxs = (Map[String, Vertex]() /: p.fs) {(m, f) => m + (f.name -> Vertex(f.name))}
-    val arcs = (List[Arc]() /: p.fs) {(a, f) => a ::: (getFreeVars(f.body).toList map {t => Arc(vxs(f.name), vxs(t.name))})}
+    val fs = p.fs.foldLeft(Map[String, Function0]())((m, f) => m + (f.name -> f))
+    val vxs =  p.fs.foldLeft(Map[String, Vertex]())((m, f) => m + (f.name -> Vertex(f.name)))
+    val arcs = p.fs.foldLeft(List[Arc]())((a, f) => a ::: (getFreeVars(f.body).toList map {t => Arc(vxs(f.name), vxs(t.name))}))
     val g = Graph(vxs.values.toList, arcs)
     val sccs = analyzeDependencies(g)
     var expr: Expression = hl0ToELC(p.goal)
     for (scc <- sccs){
-      val bs = scc.vs.toList map (x => (Variable(x.name), hl0ToELC(fs(x.name).body)))
-      if (scc.recursive){
+      val bs = scc.vs.toList.map(x => (Variable(x.name), hl0ToELC(fs(x.name).body)))
+      if (scc.recursive)
         expr = LetRecExpression(bs, expr)
-      } else {
+      else
         expr = LetExpression(bs, expr)
-      }
     }
-    //println(format(expr))
     expr
   }
 
   private def getFreeVars(expr: Expression0): Set[Variable0] = expr match {
-    case v: Variable0 => Set(v)
-    case Constructor0(_, args) => (Set[Variable0]() /: args) {(vs, term) => vs ++ getFreeVars(term)}
-    case LambdaAbstraction0(x, term) => getFreeVars(term) - x
-    case Application0(head, arg) => getFreeVars(head) ++ getFreeVars(arg)
+    case v: Variable0 =>
+      Set(v)
+    case Constructor0(_, args) =>
+      args.foldLeft(Set[Variable0]())((vs, term) => vs ++ getFreeVars(term))
+    case LambdaAbstraction0(x, term) =>
+      getFreeVars(term) - x
+    case Application0(head, arg) =>
+      getFreeVars(head) ++ getFreeVars(arg)
     case CaseExpression0(sel, bs) =>
-      getFreeVars(sel) ++ (Set[Variable0]() /: bs) {(vs, b) => vs ++ (getFreeVars(b.term) -- b.pattern.args)}
-    case LetRecExpression0(bs, expr) => getFreeVars(expr) ++ getFreeVars(bs._2) - bs._1
+      getFreeVars(sel) ++  bs.foldLeft(Set[Variable0]())((vs, b) => vs ++ (getFreeVars(b.term) -- b.pattern.args))
+    case LetRecExpression0(bs, expr) =>
+      getFreeVars(expr) ++ getFreeVars(bs._2) - bs._1
     case LetExpression0(bs, expr) =>
-      (getFreeVars(expr) /: bs) {(vs, b) => vs ++ getFreeVars(b._2)} -- (bs map {_._1})
+      bs.foldLeft(getFreeVars(expr))((vs, b) => vs ++ getFreeVars(b._2)) -- bs.map(_._1)
   }
 }
